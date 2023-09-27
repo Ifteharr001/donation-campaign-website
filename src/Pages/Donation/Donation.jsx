a0ef3a74da1105249bdc6b7b8b614a2f67e27f41
iftekharr001 <iftekharr001@gmail.com>
2023-09-27 08:03:27 -0700

    added see all button when donation card length is up to 4 then show this button

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,78 +1,134 @@
-// // import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 // import { getAddedDonation } from "../../Components/utility/localStorage";
 
 const Donation = () => {
+  const [donate, setDonate] = useState([]);
+  const [noFound, setNoFound] = useState(false);
+  const [isShow, setIsShow] = useState(false)
+  useEffect(() => {
+    const donationItems = JSON.parse(localStorage.getItem("donation"));
+    if (donationItems) {
+      setDonate(donationItems);
+    } else {
+      setNoFound("No Data Found");
+    }
+  }, []);
+  console.log(donate);
 
-    // const donationsAdded = useLoaderData();
-    
-    //     const [addDonation, setAddDonation] = useState([])
-    // useEffect(() => {
-    //     const donationApplied = [];
-    //     const storedDonetionIds = getAddedDonation();{
-    //         const donationSet = donationsAdded?.data?.filter(donation => storedDonetionIds.includes(donation.id))
-    //         if(donationSet){
-    //             donationApplied.push(donationSet)
-    //         }
-    //         setAddDonation(donationSet);
-            
-            
-    //     }
-    // }, [donationsAdded])
-    return (
+  return (
+    <div>
+      {noFound ? (
+        <div className="h-screen flex justify-center items-center font-bold md:font-extrabold text-2xl md:text-6xl">
+          <h3>No Data Found</h3>
+        </div>
+      ) : (
+        <div>
+          <div className="grid grid-cols-2 gap-8 pt-[150px]">
+            {isShow
+              ? donate.map((donates) => (
+                  <div key={donates.id}>
+                    <div
+                      className="card card-side rounded-none"
+                      style={{ backgroundColor: donates.card_bg }}
+                    >
+                      <figure>
+                        <img
+                          className="rounded-none p-4"
+                          src={donates.picture}
+                          alt="Movie"
+                        />
+                      </figure>
+                      <div className="card-body">
+                        <span
+                          className="px-2 py-2 font-bold rounded-md w-[50%] text-center"
+                          style={{
+                            backgroundColor: donates.category_card_bg,
+                            color: donates.text_button_bg,
+                          }}
+                        >
+                          {donates.category_card}
+                        </span>
+                        <h2 className="card-title text-black font-bold">
+                          {donates.title}
+                        </h2>
+                        <p
+                          className="font-bold"
+                          style={{ color: donates.text_button_bg }}
+                        >
+                          ${donates.price}
+                        </p>
+                        <div className="card-actions justify-end">
+                          <Link to={`/card/${donates.id}`}>
+                            <button className="btn bg-[#FF444A] border-none rounded-sm text-white font-bold">
+                              View Details
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : donate.slice(0, 4).map((donates) => (
+                  <div key={donates.id}>
+                    <div
+                      className="card card-side rounded-none"
+                      style={{ backgroundColor: donates.card_bg }}
+                    >
+                      <figure>
+                        <img
+                          className="rounded-none p-4"
+                          src={donates.picture}
+                          alt="Movie"
+                        />
+                      </figure>
+                      <div className="card-body">
+                        <span
+                          className="px-2 py-2 font-bold rounded-md w-[50%] text-center"
+                          style={{
+                            backgroundColor: donates.category_card_bg,
+                            color: donates.text_button_bg,
+                          }}
+                        >
+                          {donates.category_card}
+                        </span>
+                        <h2 className="card-title text-black font-bold">
+                          {donates.title}
+                        </h2>
+                        <p
+                          className="font-bold"
+                          style={{ color: donates.text_button_bg }}
+                        >
+                          ${donates.price}
+                        </p>
+                        <div className="card-actions justify-end">
+                          <Link to={`/card/${donates.id}`}>
+                            <button className="btn bg-[#FF444A] border-none rounded-sm text-white font-bold">
+                              View Details
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+          </div>
 
-      <div>
-
-      </div>
-      // <div className="grid grid-cols-2 gap-8 pt-[150px]">
-      //   {addDonation.map((donationADD) =>  (
-      //     <div key={donationADD.id}>
-      //       <div
-      //         className="card card-side rounded-none"
-      //         style={{ backgroundColor: donationADD.card_bg }}
-      //       >
-      //         <figure>
-      //           <img
-      //             className="rounded-none p-4"
-      //             src={donationADD.picture}
-      //             alt="Movie"
-      //           />
-      //         </figure>
-      //         <div className="card-body">
-      //           <span
-      //             className="px-2 py-2 font-bold rounded-md w-[50%] text-center"
-      //             style={{
-      //               backgroundColor: donationADD.category_card_bg,
-      //               color: donationADD.text_button_bg,
-      //             }}
-      //           >
-      //             {donationADD.category_card}
-      //           </span>
-      //           <h2
-      //             className="card-title"
-      //             style={{ color: donationADD.text_button_bg }}
-      //           >
-      //             {donationADD.title}
-      //           </h2>
-      //           <p
-      //             className="font-bold"
-      //             style={{ color: donationADD.text_button_bg }}
-      //           >
-      //             ${donationADD.price}
-      //           </p>
-      //           <div className="card-actions justify-end">
-      //             <Link to={`/card/${donationADD.id}`}>
-      //               <button className="btn bg-[#FF444A] border-none rounded-sm text-white font-bold">
-      //               View Details
-      //             </button>
-      //             </Link>
-      //           </div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   ))}
-      // </div>
-    );
+          <div className="my-5">
+            {donate.length > 4 && (
+              <button
+                onClick={() => setIsShow(!isShow)}
+                className="block btn bg-[#FF444A] border-none rounded-sm text-white font-bold px-8 py-3 mx-auto"
+              >
+                {isShow ? "See Less" : "See All"}
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Donation;
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
+      <div>
         <div className="h-screen flex justify-center items-center font-extrabold text-6xl">
-            <h3>Opps....</h3>
+          <div className="flex flex-col">
+            <div className="">
+              <h3>Opps...404 Error</h3>
+            </div>
+            <div className="text-center">
+              <Link to="/">
+                <button className="btn justify-center items-center font-extrabold bg-[#FF444A] border-none rounded-sm text-white px-8 py-3 ">
+                  Go Home
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
